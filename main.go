8bd66f6125f2f3a7e174db3526ab99a7ba8b9474
dannielss <daniel_sousa.p@outlook.com>
2023-05-30 00:44:50 -0300

    feat: save all repositories into mongodb collection

--- main.go
+++ main.go
@@ -2,24 +2,36 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"log"
 
+	"github.com/dannielss/go-mongodb/config"
 	"github.com/dannielss/go-mongodb/model"
 	service "github.com/dannielss/go-mongodb/services"
 )
 
-func main() {
-	var repos []model.RepoStruct
 
-	res, err := service.GetAllUserRepositories("dannielss");
+func main() {
+	repoCollection, err := config.Init()
 
 	if err != nil {
-		fmt.Print("err", err)
+		log.Fatal("err", err)
+	}
+
+	service := service.NewService(repoCollection)
+
+	var repos []model.RepoStruct
+
+	res, err := service.GetAllUserRepositories("dannielss")
+
+	if err != nil {
+		log.Fatal("err", err)
 	}
 
 	err = json.Unmarshal(res, &repos)
 
 	if err != nil {
-		fmt.Print("err", err)
+		log.Fatal("err", err)
 	}
+
+	service.SaveRepositories(repos)
 }
