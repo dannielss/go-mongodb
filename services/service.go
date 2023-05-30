1403ca9c98917691d30affdd81ba18f3e260dd29
dannielss <daniel_sousa.p@outlook.com>
2023-05-30 00:43:40 -0300

    feat: save repos into db

--- services/service.go
+++ services/service.go
@@ -1,11 +1,29 @@
 package service
 
 import (
+	"context"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/dannielss/go-mongodb/model"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func GetAllUserRepositories(user string) ([]byte, error) {
+func NewService(repoCollection *mongo.Collection) ServiceInterface {
+	return &service{repoCollection: repoCollection}
+}
+
+type ServiceInterface interface {
+	GetAllUserRepositories(user string) ([]byte, error)
+	SaveRepositories(repos []model.RepoStruct)
+}
+
+type service struct {
+	repoCollection *mongo.Collection
+}
+
+func (s *service) GetAllUserRepositories(user string) ([]byte, error) {
 	response, err := http.Get("https://api.github.com/users/"+user+"/repos")
 
 	if err != nil {
@@ -19,4 +37,14 @@ func GetAllUserRepositories(user string) ([]byte, error) {
 	}
 
 	return responseData, nil
+}
+
+func (s * service) SaveRepositories(repos []model.RepoStruct) {
+	for _, repo := range repos {
+		_, err := s.repoCollection.InsertOne(context.TODO(), repo)
+
+		if err != nil {
+			fmt.Print("err", err)
+		}
+	}
 }
